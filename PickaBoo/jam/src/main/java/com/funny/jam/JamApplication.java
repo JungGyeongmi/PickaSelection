7cea31e3344b781b000bbfae43cd0862e014a0c5
JungGyeongmi <gm950715@gmail.com>
2022-09-09 00:31:48 +0900

    create project
    이거슨 빈깡통입니다

---
+++ PickaBoo/jam/src/main/java/com/funny/jam/JamApplication.java
@@ -0,0 +1,13 @@
+package com.funny.jam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JamApplication.class, args);
+	}
+
+}
